01bb9e64acc94bab86bfad186ce864ea367a108b
giedriusliau <giedriusliaudenskas@gmail.com>
2024-03-15 09:48:52 +0200

    1-3 task done

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+// 1 uzd
+        System.out.println("----- 1 uzd -----");
+        String name = "Jonas";
+        String surname = "Jonauskas";
+        System.out.println(surname.length());
+        System.out.println(name.length());
+        if (surname.length() > surname.length()) {
+            System.out.println(surname);
+        } else {
+            System.out.println(name);
+
+        }
+
+        // 2 uzd
+        System.out.println("----- 2 uzd -----");
+        System.out.println(name.toUpperCase());
+        System.out.println(surname.toLowerCase());
+
+        // 3 uzd
+        System.out.println("----- 3 uzd -----");
+
+        String inic = (name.substring(0, 1) + surname.substring(0, 1));
+        System.out.println(name.substring(0, 1));
+        System.out.println(surname.substring(0, 1));
+        System.out.println(inic);
+
+
+        // 4 uzd
+        System.out.println("----- 4 uzd -----");
+      //  String last = ();
+
+    }
+}
+
+
